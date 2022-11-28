420c5434c40adc966616df0e7196864d86b61897
Daudi Lazarre <77812330+daudi-lazarre@users.noreply.github.com>
2022-11-28 10:40:36 -0500

    Create 10-update_uniq_items.js

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+// Function: Returns and updated map for all items in the list with initial quantity at 1.
+export default function updateUniqueItems(groceries) {
+    // Check if the argument is a map. If not, return an empty map.
+    if (Object.getPrototypeOf(groceries) !== Map.prototype) throw TypeError('Cannot process');
+  
+    groceries.forEach((val, key) => { // Iterate through the map.
+      if (val === 1) {
+        groceries.set(key, 100); // If the quantity is 1, set it to 100.
+      }
+    });
+    return groceries; // Return the updated map.
+  }
