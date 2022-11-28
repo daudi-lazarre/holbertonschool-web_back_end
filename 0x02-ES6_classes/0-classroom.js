2e72175073cba80c712a6d5d112b98fe1865d6d1
Daudi Lazarre <77812330+daudi-lazarre@users.noreply.github.com>
2022-11-28 10:23:54 -0500

    Update 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,6 @@
-export default class ClassRoom {
-    constructor(maxStudentsSize = 0) {
-      this._maxStudentsSize = maxStudentsSize;
+export default class ClassRoom { // Prototype class for ClassRoom
+    // Constructor function for ClassRoom class with parameter maxStudentsSize (number)
+      constructor(maxStudentsSize) {
+        this._maxStudentsSize = maxStudentsSize; // Private variable for maxStudentsSize
+      }
     }
-  }
